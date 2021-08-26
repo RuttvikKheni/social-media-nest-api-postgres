856c65adbe2ebdeec30770c7b5db4baa7e6fa14b
ruttvikkheni <khenirutvik2001@gmail.com>
2021-08-26 23:21:03 +0530

    configure postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/config.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()), UserModule],
+  controllers: [UserController],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/config/config.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { configService } from './config.service';
+
+@Module({
+})
+export class ConfigModule { }
